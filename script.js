d5dd7fe251bc18144f3513a9689845119e0e1a60
SouravvMishra <souravvmishra@gmail.com>
2022-08-28 23:00:24 +0530

    First Commit , Document IS completed Till My Team Section

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  const hamburger = document.querySelector("#hamburger");
+  const cross = document.querySelector("#cross");
+  console.log("Hello")
+//   const hamanim = document.querySelector("#ham-anim");
+//   const crossanim = document.querySelector("#cross-anim");
+//   const mobmenu = document.querySelector("#mob-menu")
+
+  hamburger.onclick = function () {
+    console.log("Clicked");
+  };
+
+  cross.onclick = function () {
+    
+  };
+
+
+
+ 
