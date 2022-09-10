512b32bd0f669a2f78a7aff2f22748f35bf30721
SouravvMishra <souravvmishra@gmail.com>
2022-09-10 19:10:38 +0530

    Did somethings like changed bg color & image

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 
-  const hamburger = document.querySelector("#hamburger");
-  const cross = document.querySelector("#cross");
-  console.log("Hello")
-//   const hamanim = document.querySelector("#ham-anim");
-//   const crossanim = document.querySelector("#cross-anim");
-//   const mobmenu = document.querySelector("#mob-menu")
-
-  hamburger.onclick = function () {
-    console.log("Clicked");
-  };
-
-  cross.onclick = function () {
-    
-  };
 
 
 
- 
+
+
